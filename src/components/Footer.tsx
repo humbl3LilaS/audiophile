c4adbe150fefa8296047fd3b2181162262e2f5ea
humbl3LilaS <sabishinekobebe@gmail.com>
2024-10-23 20:31:25 +0630

    fix: layout && hover state in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 const Footer = () => {
 	return (
 		<footer className="px-6 py-14 relative bg-darkBlack md:px-10 lg:px-40">
-			<nav>
+			<nav className=" lg:flex justify-between items-center">
 				<img
 					src="/icons/logo.svg"
 					alt="logo"
 					className="mx-auto mb-12 md:mx-0"
 				/>
-				<ul className="w-full mb-12 flex flex-col items-center justify-center gap-y-4 text-white font-bold uppercase md:flex-row md:gap-x-9 md:gap-y-0 md:justify-start">
-					<li>
+				<ul className="w-full mb-12 flex flex-col items-center justify-center gap-y-4 text-white font-bold uppercase md:flex-row md:gap-x-9 md:gap-y-0 md:justify-start lg:justify-end">
+					<li className="transition-colors duration-500 hover:text-primary">
 						<Link to="/">Home</Link>
 					</li>
-					<li>
+					<li className="transition-colors duration-500 hover:text-primary">
 						<Link to="/headphones">Headphones</Link>
 					</li>
-					<li>
+					<li className="transition-colors duration-500 hover:text-primary">
 						<Link to="/speakers">Speakers</Link>
 					</li>
-					<li>
+					<li className="transition-colors duration-500 hover:text-primary">
 						<Link to="/earphones">Earphones</Link>
 					</li>
 				</ul>
@@ -35,29 +35,35 @@ const Footer = () => {
 					Copyright {new Date().getFullYear()}. All rights Reserved
 				</p>
 				<nav className="lg:order-2 lg:mb-12">
-					<li className="w-full flex items-center justify-center gap-x-4">
-						<Link to="/facebook">
-							<img
-								src="/icons/icon-facebook.svg"
-								alt="facebook"
-							/>
-						</Link>
-						<Link to="/twitter">
-							<img
-								src="/icons/icon-twitter.svg"
-								alt="twitter"
-							/>
-						</Link>
-						<Link to="/instagram">
-							<img
-								src="/icons/icon-instagram.svg"
-								alt="instagram"
-							/>
-						</Link>
-					</li>
+					<ul className="w-full flex items-center justify-center gap-x-4">
+						<li>
+							<Link to="/facebook">
+								<img
+									src="/icons/icon-facebook.svg"
+									alt="facebook"
+								/>
+							</Link>
+						</li>
+						<li>
+							<Link to="/twitter">
+								<img
+									src="/icons/icon-twitter.svg"
+									alt="twitter"
+								/>
+							</Link>
+						</li>
+						<li>
+							<Link to="/instagram">
+								<img
+									src="/icons/icon-instagram.svg"
+									alt="instagram"
+								/>
+							</Link>
+						</li>
+					</ul>
 				</nav>
 			</div>
-			<span className="w-[100px] h-1 absolute top-0 left-1/2 -translate-x-1/2 bg-primary md:left-10 md:-translate-x-0" />
+			<span className="w-[100px] h-1 absolute top-0 left-1/2 -translate-x-1/2 bg-primary md:left-10 md:-translate-x-0 lg:left-[160px]" />
 		</footer>
 	);
 };
