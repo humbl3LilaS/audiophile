3bb24d928b57fdb2db3f9f829bd1710ce7ccf885
humbl3LilaS <sabishinekobebe@gmail.com>
2024-10-23 11:39:45 +0630

    add: assets and theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#D87D4A",
+				secondary: "#FBAF85",
+				text: "#101010",
+				paleGray: "#F1F1F1",
+				paleWhite: "#FAFAFA",
+			},
+		},
 	},
 	plugins: [],
 };
